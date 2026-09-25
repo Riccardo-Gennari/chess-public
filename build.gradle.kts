b314540c3a7685d7622010f1cb06086fc62678ad
Riccardo Gennari <riccardo.gennari@nbf.it>
2026-09-25 15:55:39 +0200

    build: integrate Detekt static analysis and update code quality setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,32 @@
 import it.ric.convention.setupQualityChecks
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
 
 plugins {
     id("it.ric.convention")
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.googleServices) apply false
+
+    alias(libs.plugins.detekt) apply true
+    alias(libs.plugins.ktlint) apply true
     alias(libs.plugins.semanticVersioning) apply true
 }
 
+dependencies {
+    "detektPlugins"(libs.detekt.compose.rules)
+    "ktlintRuleset"(libs.ktlint.compose.rules)
+}
+
+configure<KtlintExtension> {
+    version.set(libs.versions.ktlint.core)
+}
+
 allprojects {
-    setupQualityChecks()
+    setupQualityChecks(
+        detektPlugin = rootProject.libs.plugins.detekt,
+        ktlintPlugin = rootProject.libs.plugins.ktlint,
+        detektRules = rootProject.libs.detekt.compose.rules,
+        ktlintRules = rootProject.libs.ktlint.compose.rules,
+        ktlintVersion = rootProject.libs.versions.ktlint.core,
+    )
 }
